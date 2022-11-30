82587992f11962230d5d27473a8ff6623d90dec8
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2022-11-30 23:07:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
+      BOT_TOKEN = "5762860961:AAHmkOuM23UibNupEbSnig9CjObJtYDIdJg"
+      API_ID = 5080899
+      API_HASH = "6fc7f813cf96e6692990b752b43fd9c7"
+      CAPTION_POSITION = "bottom"
+      ADMIN_USERNAME = hellohoney
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
       DB_URL = os.environ.get("DATABASE_URL", "")
